5fc918d8dea9fed8f23ccd8f03a7e7de464418c7
Viktor <viktorbezai@gmail.com>
2023-07-25 10:29:08 -0400

    Chapter 12

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,11 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import generics, permissions
+from rest_framework.authtoken.models import Token
+from rest_framework.parsers import JSONParser
 
 from todo.models import Todo
 from .serializers import TodoSerializer, TodoToggleCompleteSerializer
@@ -36,3 +43,40 @@ class TodoToggleComplete(generics.UpdateAPIView):
     def perform_update(self, serializer):
         serializer.instance.completed = not serializer.instance.completed
         serializer.save()
+
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)
+            user = User.objects.create_user(
+                username=data['username'],
+                password=data['password'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse(
+                {'error': 'username taken. choose another username'},
+                status=400)
+
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(
+            request,
+            username=data['username'],
+            password=data['password'])
+        if user is None:
+            return JsonResponse(
+                {'error': 'unable to login. check username and password'},
+                status=400)
+        else:
+            try:
+                token = Token.objects.get(user=user)
+            except:
+                token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
